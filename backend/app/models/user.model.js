1f7208811a02fdf4533375c9376309eae1309d61
Ryan Byloos <ryan.byloos@posteo.net>
2022-05-05 22:04:23 +0200

    typo

--- backend/app/models/user.model.js
+++ backend/app/models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const User = mongoose.model(
     "User",
     new mongoose.Schema({
-        name: String,
+        firstname: String,
         lastname: String,
         email: String,
         password: String,
