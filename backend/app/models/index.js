155abac631b2791f0a57ca4eb9b4a07446e120de
ryanbyloos <ryan.byloos@posteo.net>
2022-05-07 09:56:52 +0200

    improved api and auth working

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const db = {};
+
 db.mongoose = mongoose;
 db.user = require("./user.model");
 db.role = require("./role.model");
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,66 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./app/models");
+const dbConfig = require("./app/config/db.config");
+
+const Role = db.role;
+
 const app = express();
+
+
+
 var corsOptions = {
   origin: "http://localhost:8081"
 };
+
 app.use(cors(corsOptions));
-// parse requests of content-type - application/json
 app.use(express.json());
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-// simple route
 app.get("/", (req, res) => {
   res.json({ message: "Hello world !" });
 });
+
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch(err => {
+    console.log("Could not connect to the database. Exiting now...", err);
+    process.exit();
+  });
+
+function dbInit() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'user' to roles collection");
+      });
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
