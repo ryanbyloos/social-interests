583ed4067a3b8bac8b9ee3bbaa4a9c9a4b30226e
ryanbyloos <ryan.byloos@posteo.net>
2022-06-01 22:01:12 +0200

    it will work, eventually

--- frontend/src/api/userAPI.js
+++ frontend/src/api/userAPI.js
@@ -1,6 +1,5 @@
-// export const API_HOST =
-// process.env.REACT_APP_API_HOST || "http://social.ryanb.be:8080";
-export const API_HOST = "http://social.ryanb.be:8080";
+export const API_HOST =
+  process.env.REACT_APP_API_HOST || "http://social.ryanb.be:8080";
 
 export async function whoami() {
   const res = await fetch(`${API_HOST}/api/auth/whoami`, {
